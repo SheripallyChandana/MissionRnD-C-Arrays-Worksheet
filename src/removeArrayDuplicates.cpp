edbfc62cb6e2fb4072a1673c50e749c939813628
SheripallyChandana <sheripallychandana@hotmail.com>
2015-11-09 20:26:51 +0530

    Submit Commit

--- src/removeArrayDuplicates.cpp
+++ src/removeArrayDuplicates.cpp
@@ -12,8 +12,49 @@ NOTES: Don't create new array, try to change the input array.
 */
 
 #include <stdio.h>
+int findBucket(int key, int* numbers, int* hashTable, int bucketSize, int tableSize){
+	
+	int bucket = key % tableSize;
+	bucket = bucket < 0 ? -bucket : bucket;
+	int tempBucket = bucket;
+	int i;
+	
+	if(hashTable[bucket] == -1)
+		return bucket;
+	else		
+		for(i = 0; hashTable[bucket] != -1 && tableSize != bucketSize; ++i){
+			bucket = (tempBucket + i) % tableSize;
+			if(hashTable[bucket] == key)
+				return bucket;
+		}
+		return bucket;	
+}
 
 void * removeArrayDuplicates(int *Arr, int len)
 {
-	return NULL;
+	if(Arr == NULL || len < 1)
+		return NULL;
+
+	int index;
+	int bucket;
+	int hashTable[30];
+	int bucketSize = 0;
+	int tempIndex = 0;
+	
+	for(index = 0; index < len; ++ index)
+		hashTable[index] = -1;
+	
+	for (index = 0; index < len; ++index){
+		
+		bucket = findBucket(Arr[index], Arr, hashTable, bucketSize, 30);
+		if(hashTable[bucket] == Arr[index])
+			continue;
+		else{		
+			hashTable[bucket] = Arr[index];
+			Arr[tempIndex++] = Arr[index];
+			bucketSize++;
+		}
+	}
+	
+	return Arr;
 }
