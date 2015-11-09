286268fb81c44798647c482f9b66d1f83f16e1dc
SheripallyChandana <sheripallychandana@hotmail.com>
2015-11-09 20:24:05 +0530

    Submit Commit

--- src/scoresDescendingSort.cpp
+++ src/scoresDescendingSort.cpp
@@ -20,6 +20,71 @@ struct student {
 	int score;
 };
 
+void swap(struct student* a, struct student* b){
+	
+	struct student temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;	
+}
+void stringCopy(char* string1, char* string2){
+
+	int index = 0;
+	while (string1[index] != '\0'){
+		string2[index] = string1[index];
+		index++;
+	}
+	string2[index] = '\0';
+}
+
+
+void qSort(struct student* numbers, int leftIndex, int rightIndex){
+	
+	int leftHold  = leftIndex;
+	int rightHold = rightIndex;
+	int midIndex  = (leftIndex + rightIndex) / 2;
+	
+	swap(&numbers[leftIndex], &numbers[midIndex]);
+	
+	int pivotIndex;
+	int pivot = numbers[leftIndex].score;
+	char temp[10];
+	stringCopy(numbers[leftIndex].name, temp);
+	
+	
+	while(leftIndex < rightIndex){
+		while( numbers[rightIndex].score <= pivot && leftIndex < rightIndex )
+			--rightIndex;
+		if(leftIndex != rightIndex){
+			stringCopy(numbers[rightIndex].name,numbers[leftIndex].name);
+			numbers[leftIndex].score = numbers[rightIndex].score;
+			leftIndex++;
+		}	
+		while(numbers[leftIndex].score >= pivot && leftIndex < rightIndex )	
+			++leftIndex;
+		if(leftIndex != rightIndex){
+
+			stringCopy(numbers[leftIndex].name,numbers[rightIndex].name);
+			numbers[rightIndex].score = numbers[leftIndex].score;
+			rightIndex--;
+		}
+	}
+	
+	numbers[leftIndex].score = pivot;
+	stringCopy(temp, numbers[leftIndex].name);
+
+	pivotIndex = leftIndex;
+	if(leftHold < pivotIndex)
+		qSort(numbers ,leftHold, pivotIndex - 1);
+	if(rightHold > pivotIndex)
+		qSort(numbers , pivotIndex + 1, rightHold);	
+}
 void * scoresDescendingSort(struct student *students, int len) {
-	return NULL;
+
+	if(students == NULL || len < 1)
+		return NULL;
+		
+	qSort(students, 0, len-1);
+
+	return students;
 }
